9d71536ed13ed274ec67d23601b74799330d0ec4
rogerjeffs <roger.jeffs@sf-nett.no>
2023-12-31 17:38:00 +0100

    Deleted unused files

--- src/hooks/useList.jsx
+++
@@ -1,6 +0,0 @@
-import useAxios from "axios-hooks";
-function useList() {
-  const [{ data, loading, error }] = useAxios("/data/list_data.json");
-  return { data, loading, error };
-}
-export default useList;
