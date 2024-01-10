92408a3f235d44f707ef2fd5e88a9925d5d89068
rogerjeffs <roger.jeffs@sf-nett.no>
2024-01-10 18:02:43 +0100

    tweaked vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: false,
   plugins: [react()],
-  // base: "./",
+  build: {
+    base: "./",
+    publicDir: "./test",
+  },
 });
